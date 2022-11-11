5c0ca9b5adf5e2f11915822d740c640df85b6ac5
Nik92152 <extend52@yandex.ru>
2022-11-11 21:33:56 +0300

    While

--- app.js
+++ app.js
@@ -10,7 +10,33 @@
 //     console.log("Очко")
 // }
 
-const WhoisUser = "";
+// const WhoisUser = "";
 
-const typeofsuer = WhoisUser ? "Администратор": "Пользователь";
-console.log(typeofsuer);
+// const typeofsuer = WhoisUser ? "Администратор": "Пользователь";
+// console.log(typeofsuer);
+
+// const User="Admin".toLowerCase();
+
+// switch (User) {
+//     case ("admin"):
+//         console.log("Yoy're admin");
+//         break;
+//     case "user":
+//         console.log("Yoy're user");
+//         break;
+//     case "bot":
+//         console.log("Yoy're bot");
+//         break;
+//     default:
+//         console.log("Who are you, warrior?");
+//         break;
+// }
+
+let i=0;
+const ln=10;
+let message='';
+while(i<ln){
+    i++;
+    message +=`${i},`;
+}
+console.log(message);
