049fc79d295293263d8109d2529d2b3168dcc1fc
Nik92152 <extend52@yandex.ru>
2022-11-11 14:30:31 +0300

    Тернарный оператор

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-let age = null;
+// let age = null;
 
-if (!isNaN(age)) {
-    console.log(" Выпей пивка");
-}
-else if (age<21){
-    console.log("Иди домой");
-}
-else {
-    console.log("Очко")
-}
+// if (!isNaN(age)) {
+//     console.log(" Выпей пивка");
+// }
+// else if (age<21){
+//     console.log("Иди домой");
+// }
+// else {
+//     console.log("Очко")
+// }
+
+const WhoisUser = "";
+
+const typeofsuer = WhoisUser ? "Администратор": "Пользователь";
+console.log(typeofsuer);
